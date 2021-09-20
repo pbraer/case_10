67368877112c14c36361c4eabdab6c9ed1b5c5f9
pbraer <73088589+pbraer@users.noreply.github.com>
2021-09-20 11:15:48 +0700

    machines info

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 # Developers:   Braer P. (%),
 #               Kokorina D. (%),
 #               Zhuravlev A. (%)
+
+# Information about machines
+with open('azs.txt', 'r') as info:
+    to_read = info.readlines()
+    azs_info = {}
+    for line in to_read:
+        inf = []
+        number = line[:line.find(' ')]
+        line = line[line.find(' ') + 1:]
+        q_max = line[:line.find(' ')]
+        line = line[line.find(' ') + 1:]
+        inf.append(int(q_max))
+        petrol = line[:line.find('\n')]
+        if petrol.find(' ') != -1:
+            petrol = petrol.split(' ')
+            petrol = set(petrol)
+        inf.append(petrol)
+        azs_info[number] = inf
+print(azs_info)
