826a8c877a166d5d016338b77a5f825ddf727155
pbraer <73088589+pbraer@users.noreply.github.com>
2021-09-20 21:28:47 +0700

    correct

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print('hi')
 # Case-study №9 / Gas station operation model
 # Developers:   Braer P. (%),
 #               Kokorina D. (%),
@@ -21,3 +22,23 @@ with open('azs.txt', 'r') as info:
         inf.append(petrol)
         azs_info[number] = inf
 print(azs_info)
+
+
+class GasStation:
+    def __init__(self, number, queue_len, gas_types, queue=0, time=0, litres=0, client_gas=0):
+        self.number = number
+        self.queue_len = queue_len
+        self.gas_types = gas_types
+        self.queue = queue
+        self.time = time
+        self.litres = litres
+        self.client_gas = client_gas
+
+    def __repr__(self):
+        return 'Автомат №'+self.number + ' Максимальная очередь: ' + self.queue_len + ' Марки бензина:' + self.gas_types + '->' + self.queue
+
+
+a = GasStation('1', '3', '98, 92, 95', '1')
+print(a)
+
+
