8fe740a95bafadb15d86c4cefa9532930b0b5f86
darya752 <73088666+darya752@users.noreply.github.com>
2021-09-27 06:50:24 +0300

    %

--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,19 @@
 # Case-study №9 / Gas station operation model
-# Developers:   Braer P. (%),
-#               Kokorina D. (%),
+# Developers:   Braer P. (80%),
+#               Kokorina D. (30%),
 #               Zhuravlev A. (%)
 
-from random import randint
+import random
+import math
 
-# petrol price
-price = {'АИ-92': 45.02, 'АИ-80': 31.50, 'АИ-98': 57.72}
+#petrol price
+
+price = {'АИ-92': 45.02, 'АИ-95': 31.50, 'АИ-98': 57.72}
+
+# start param
+money_sum = 0
+fail_clients = 0
+amount_petrol = {'АИ-92': 0, 'АИ-95': 0, 'АИ-98': 0}
 
 # Information about machines
 with open('azs.txt', 'r') as info:
@@ -24,11 +31,10 @@ with open('azs.txt', 'r') as info:
             petrol = petrol.split(' ')
             petrol = set(petrol)
         inf.append(petrol)
+        inf.append('')
         azs_info[number] = inf
 
 # Information about day
-# print(azs_info)
-# print(number)
 
 with open('input.txt', 'r') as info:
     to_read = info.readlines()
@@ -59,9 +65,8 @@ def new_client_done(hour, minute, petrol, litres, need_minutes, number):
     minute = str(minute)
     if len(minute) == 1:
         minute = '0' + minute
-    print('В ', hour, ':', minute, ' новый клиент:  ', hour, ':', minute, ' ', petrol, ' ', litres, ' ', need_minutes,
-          ' встал в очередь к автомату №', number, sep='')
-
+    done = 'В ' + hour + ':' + minute +' новый клиент: ' + hour + ':' + minute + ' ' + petrol + ' ' + litres + ' ' + need_minutes + ' встал в очередь к автомату №' + number
+    return done
 
 def new_client_fail(hour, minute, petrol, litres, need_minutes):
     hour = str(hour)
@@ -70,92 +75,123 @@ def new_client_fail(hour, minute, petrol, litres, need_minutes):
     minute = str(minute)
     if len(minute) == 1:
         minute = '0' + minute
-    print('В ', hour, ':', minute, ' новый клиент:  ', hour, ':', minute, ' ', petrol, ' ', litres, ' ', need_minutes,
-          ' не смог заправить автомобиль и покинул АЗС.', sep='')
+    fail = 'В ' + hour + ':' + minute + ' новый клиент:  ' + hour + ':' + minute + ' ' + petrol + ' ' + litres + ' ' + need_minutes + ' не смог заправить автомобиль и покинул АЗС.'
+    return fail
 
 
-# new_client_done(1, 1, 'АИ-68', 50, 4, 3)
-
+def new_client_go(new_hour, new_minute, hour, minute, petrol, litres, need_minutes):
+    go = 'В ' + new_hour + ':' + new_minute + ' клиент: ' + hour + ':' + minute + ' ' + petrol + ' ' + litres + ' ' + need_minutes + ' заправил свой автомобиль и покинул АЗС.'
+    return go
 
 # Writing about information of machine at the moment
 
 def machine_info(number, max_queue, petrol, amount):
-    print('Автомат №', number, ' максимальная очередь: ', max_queue, ' Марки бензина: ', petrol, ' ->', amount, sep='')
+    m_inf = 'Автомат №' + number + ' максимальная очередь: ' + max_queue + ' Марки бензина: ' + petrol + ' ->' + amount
+    return m_inf
 
-
-# machine_info(3, 4, 'АИ-68', '**')
 
 def result(sum_litres, sum, lose):
-    print('Количество литров, проданное за сутки по каждой марке бензина: ', sum_litres, sep='')
-    print('Общая сумма продаж за сутки: ', sum, sep='')
-    print('Количество клиентов, которые покинули АЗС не заправив автомобиль из-за «скопившейся» очереди: ', lose,
-          sep='')
+        print('Количество литров, проданное за сутки по каждой марке бензина: ', sum_litres, sep='')
+        print('Общая сумма продаж за сутки: ', sum, sep='')
+        print('Количество клиентов, которые покинули АЗС не заправив автомобиль из-за «скопившейся» очереди: ', lose, sep='')
 
 
-def m_info(number, max_queue, petrol, amount):
-    print('Автомат №', number, ' максимальная очередь: ', max_queue, ' Марки бензина: ', petrol, ' ->', amount, sep='')
 
 
-with open('input.txt', 'r') as info:
-    to_read = info.readlines()
-    day_info = []
-    for line in to_read:
-        inf = []
-        hours = line[:line.find(':')]
-        hours = int(hours)
-        inf.append(hours)
-        minutes = line[line.find(':') + 1: line.find(' ')]
-        minutes = int(minutes)
-        inf.append(minutes)
-        line = line[line.find(' ') + 1:]
-        litres = line[:line.find(' ')]
-        litres = int(litres)
-        inf.append(litres)
-        petrol = line[line.find(' ') + 1: line.find('\n')]
-        inf.append(petrol)
-        day_info.append(inf)
+result_today = {}
+now = []
 
-#new_client_done(1, 1, 5, 50, 5, 'АИ-92')
+for client in day_info:
+    h_time = str(client[0])
+    if len(h_time) == 1:
+        h_time = '0' + h_time
+    m_time = str(client[1])
+    if len(m_time) == 1:
+        m_time = '0' + m_time
+    before = len(result_today)
+    for machine in azs_info:
+        inf = azs_info[machine]
+        if 0 <= len(inf[2]) <= inf[0] and (client[3] in inf[1] or client[3] == inf[1]) and client[3] in price:
+            need_min = math.ceil(client[2]/10 + random.randint(-1, 1))
+            if need_min == 0:
+                need_min = 1
+            done = new_client_done(h_time, m_time, str(client[3]), str(client[2]), str(need_min), str(machine))
+            now =[]
+            for avt in azs_info:
+                info = azs_info[avt]
+                if len(info[1]) == 3:
+                    petrol = ''
+                    for el in info[1]:
+                        petrol = petrol + el + ' '
+                    petrol = petrol[:len(petrol) - 1]
+                else:
+                    petrol = info[1]
+                m_inf = machine_info(str(avt), str(info[0]), str(petrol), str(inf[2] + '*'))
+                now.append(m_inf)
+            inf[2] = inf[2] + '*'
+            result_today[done] = now
+            old_h = h_time
+            old_m = m_time
+            m_time = client[1] + need_min
+            h_time = client[0]
+            if m_time >= 60:
+                m_time -= 60
+                h_time += 1
+            m_time = str(m_time)
+            h_time = str(h_time)
+            if len(h_time) == 1:
+                h_time = '0' + h_time
+            if len(m_time) == 1:
+                m_time = '0' + m_time
+            now = []
+            for avt in azs_info:
+                info = azs_info[avt]
+                if len(info[1]) == 3:
+                    petrol = ''
+                    for el in info[1]:
+                        petrol = petrol + el + ' '
+                    petrol = petrol[:len(petrol) - 1]
+                else:
+                    petrol = info[1]
+                if info[2].find('*') != -1:
+                    info[2] = info[2][: info[2].find('*') - 2]
+                m_inf = machine_info(str(avt), str(info[0]), str(petrol), str(inf[2]))
+                now.append(m_inf)
+            go = new_client_go(h_time, m_time, old_h, old_m, str(client[3]), str(client[2]), str(need_min))
+            inf[2] = inf[2] + random.randint(0, 1) * '*'
+            result_today[go] = now
+            money_sum = money_sum + price[client[3]] * client[2]
+            amount_petrol[client[3]] = amount_petrol[client[3]] + client[2]
+            break
+    if len(result_today) == before:
+        now =[]
+        for avt in azs_info:
+            info = azs_info[avt]
+            if len(info[1]) == 3:
+                petrol = ''
+                for el in info[1]:
+                    petrol = petrol + el + ' '
+                petrol = petrol[:len(petrol) - 1]
+            else:
+                petrol = info[1]
+            m_inf = machine_info(str(avt), str(info[0]), str(petrol), str(info[2]))
+            now.append(m_inf)
+        fail = new_client_fail(h_time, m_time, str(client[3]), str(client[2]), str(math.ceil(client[2] / 10 + random.randint(-1, 1))))
+        result_today[fail] = now
+        fail_clients += 1
 
+result_list = list(result_today.keys())
+result_list.sort()
 
-with open('input.txt', 'r') as info:
-    to_read = info.readlines()
-    day_info = []
-    for line in to_read:
-        inf = []
-        hours = line[:line.find(':')]
-        hours = int(hours)
-        inf.append(hours)
-        minutes = line[line.find(':') + 1: line.find(' ')]
-        minutes = int(minutes)
-        inf.append(minutes)
-        line = line[line.find(' ') + 1:]
-        litres = line[:line.find(' ')]
-        litres = int(litres)
-        inf.append(litres)
-        petrol = line[line.find(' ') + 1: line.find('\n')]
-        inf.append(petrol)
-        day_info.append(inf)
+for step in result_list:
+    print('')
+    data = result_today[step]
+    print(step)
+    for avt in data:
+        print(avt)
+    print(' ')
 
-        new_client_done(hours, minutes, line, litres, 5, petrol)
-
-        with open('azs.txt', 'r') as info:
-            to_read = info.readlines()
-            azs_info = {}
-            for line in to_read:
-                inf = []
-                number = line[:line.find(' ')]
-                line = line[line.find(' ') + 1:]
-                q_max = line[:line.find(' ')]
-                line = line[line.find(' ') + 1:]
-                inf.append(int(q_max))
-                petrol = line[:line.find('\n')]
-                # if petrol.find(' ') != -1:
-                # petrol = petrol.split(' ')
-                # petrol = set(petrol)
-                # inf.append(petrol)
-                # azs_info[number] = inf
-
-                machine_info(number, q_max, petrol, '*')
-
-
+litres = ''
+for amount in amount_petrol:
+    litres = litres + amount + ': ' + str(amount_petrol[amount]) + ' '
+result(litres, money_sum, fail_clients)
